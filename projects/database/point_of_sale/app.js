2e38471ec90330a5114dedfa65a69a02a5736321
Yossi <yosileyid@gmail.com>
2020-07-22 21:45:26 -0400

    rewrote add_hourly_employee() function

--- projects/database/point_of_sale/app.js
+++ projects/database/point_of_sale/app.js
@@ -6,10 +6,17 @@ const add_hourly_employee = (first, last, role, pay) => {
     
 	let employee = new Hourly.Employee(first, last, role, pay)
 
-	fs.writeFile("./employee-db.json", employee, function() {
-		console.log( JSON.stringify(data) )
-	})
+	if(data[employee.first_name+"_"+employee.last_name]){
+		console.log(JSON.stringify(data))
+	} else {
+	    data[employee.first_name+"_"+employee.last_name] = employee
+	
+	    fs.writeFile("./employee-db.json", JSON.stringify(data, 2), function() {
+		    console.log( data )
+		})
+	}
 }
+
 
 add_hourly_employee( "John", "Smith", "hourly", 12 )
 add_hourly_employee( "James", "Smith", "hourly", 15 )
